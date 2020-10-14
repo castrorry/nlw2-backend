d02e718f17647005aa78dd0bb7cb938e22dc8561
Rayan Castro <castrorry@iMac-de-Rayan.local>
2020-10-14 13:24:58 -0300

    add routes file and controllers

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 
 import './database/connection';
+import { routes } from './routes';
 
 const app = express();
 app.use(express.json());
-
-app.post('/orphanages', (request, response) => {
-  return response.json({message: 'Hello, world'});
-});
+app.use(routes);
   
 app.listen(3333);
